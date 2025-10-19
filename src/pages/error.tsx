ba05e51e2f813ea44d5fee5895783151ebfae963
rramires <ricardo@flexbr.com>
2025-10-18 23:50:29 -0300

    Add React Helmet Async.

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async'
 import { useRouteError } from 'react-router-dom'
 
 interface RouteError {
@@ -10,12 +11,15 @@ export function ErrorPage() {
 	console.error(error)
 
 	return (
-		<div id='error-page'>
-			<h1>Oops!</h1>
-			<p>Desculpe, ocorreu um erro.</p>
-			<p>
-				<i>{error.statusText || error.message}</i>
-			</p>
-		</div>
+		<>
+			<Helmet title='Error' />
+			<div id='error-page'>
+				<h1>Oops!</h1>
+				<p>Desculpe, ocorreu um erro.</p>
+				<p>
+					<i>{error.statusText || error.message}</i>
+				</p>
+			</div>
+		</>
 	)
 }
